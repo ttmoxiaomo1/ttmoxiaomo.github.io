fc4420f645b96845759c7d3526731900729f3cc2
ttmoxiaomo1 <542339926@qq.com>
2016-10-09 15:52:11 +0800

    10/9

--- js/common.js
+++ js/common.js
@@ -1,3 +1,47 @@
 /**
  * Created by huanghaitao on 2016/10/8.
  */
+
+
+
+
+function calc(){
+    var $length=0;
+    function getData(event){
+        var e = event || window.event;
+//获取滚动距离(FF每次滚动 data为3或者-3，其他为120或者-120)
+        var data = e.detail || e.wheelDelta;
+        $length += data;
+        console.log(data,$length);
+//当鼠标开始往下滚动时候;隐藏头部
+        if(data>0){
+            document.getElementById("header").style.top="-80px";
+        }
+        var $ul= document.getElementsByTagName("ul");
+        var obj= document.getElementsByTagName("ul")[0].getElementsByTagName("li");
+        var $objlen=obj.length;
+        for(var i=0;i<$objlen;i++){
+            obj[i].style.top= (-20)*$length+"px";
+        }
+
+        console.log(obj[0].style.top);
+    }
+//IE之外的绑定事件方法
+    if(document.addEventListener && !document.attachEvent)
+    {
+        document.addEventListener('mousewheel',getData);
+//FF绑定滚动事件
+        document.addEventListener('DOMMouseScroll',getData);
+    }
+//IE
+    else if(document.attachEvent && !document.addEventListener){
+        document.attachEvent('onmousewheel',getData);
+    }else{
+        window.onmousewheel = getData;
+    }
+
+}
+
+calc();
+
+
